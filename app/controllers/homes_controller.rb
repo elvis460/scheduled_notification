9db1f64cc2bc86e5f21eadb988201c527ed3dd07
Villager <elvis4600@gmail.com>
2016-03-30 16:56:05 +0800

    modify

--- app/controllers/homes_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomesController < ApplicationController
-
-	def index
-		require 'rest-client'
-		RestClient.post 'http://im.kayac.com/api/post/elvis' ,message: 'Hello World!', content_type: 'application/x-www-form-urlencoded' 
-	end
-end
