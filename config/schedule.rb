847825367177e6e557584b395a81f297c75ec6f2
Villager <elvis4600@gmail.com>
2016-04-01 00:36:43 +0800

    schedule_update

---
+++ config/schedule.rb
@@ -0,0 +1,24 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+set :environment, 'development'
+set :output, "log/cron_log.log"
+#
+every 1.day, at: '9:00am' do
+  rake "events:breakfast"
+end
+every 1.day, at: '12:00pm' do
+  rake "events:lunch"
+end
+every 1.day, at: '6:00pm' do
+  rake "events:dinner"
+end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
