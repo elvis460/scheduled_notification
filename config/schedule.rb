68333d6c3d3fe3fb3141334f66e342c7142b0d07
Villager <elvis4600@gmail.com>
2017-08-15 17:23:52 +0800

    try fix the timezone issue

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,21 +2,23 @@
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
+require "tzinfo"
+def local(time); TZInfo::Timezone.get("Asia/Taipei").local_to_utc(Time.parse(time)); end
 
 # Example:
 set :environment, 'development'
 set :output, "log/cron_log.log"
 #
-every 1.day, at: '9:00am' do
+every 1.day, at: local('9:00am') do
   rake "events:breakfast"
 end
-every 1.day, at: '12:30pm' do
+every 1.day, at: local('12:30pm') do
   rake "events:lunch"
 end
-every 1.day, at: '6:30pm' do
+every 1.day, at: local('6:30pm') do
   rake "events:dinner"
 end
-every 1.day, at: '11:58pm' do
+every 1.day, at: local('11:58pm') do
   rake "log:clear"
 end
 #
